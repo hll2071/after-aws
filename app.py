2afcb91d34ed4418e782ae6615d9585433669ff0
bssmMrchoi <teacher009@bssm.hs.kr>
2024-05-20 16:00:54 +0900

    feat upload files to s3

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, Response
+import boto3
+from flask import Flask, request, Response
 import pymysql
 import config
 
 app = Flask(__name__)
 con = config.Config()
+s3 = boto3.client('s3',
+                  aws_access_key_id=con.AWS_ACCESS_KEY,
+                  aws_secret_access_key=con.AWS_SECRET_KEY)
 
 @app.route('/')
 def index():
@@ -13,6 +17,13 @@ def index():
 def health():
     return Response("Success Health Check", status=200)
 
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    file = request.files['file']
+    if file:
+        s3.upload_fileobj(file, con.BUCKET_NAME, file.filename)
+        return Response("upload successfully", status=200)
+    return Response("No file", status=404)
 
 if __name__ == '__main__':
     db = pymysql.connect(host=con.DB_HOST, user=con.DB_USER, password=con.DB_PASSWORD, db=con.DB_SCHEMA)
--- config.py
+++ config.py
@@ -3,8 +3,13 @@ import os
 
 load_dotenv()
 
+
 class Config:
-    DB_HOST=os.environ.get('DB_HOST')
-    DB_USER=os.environ.get('DB_USER')
-    DB_PASSWORD=os.environ.get('DB_PASSWORD')
-    DB_SCHEMA=os.environ.get('DB_SCHEMA')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_SCHEMA = os.environ.get('DB_SCHEMA')
+
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    BUCKET_NAME = os.environ.get('BUCKET_NAME')
